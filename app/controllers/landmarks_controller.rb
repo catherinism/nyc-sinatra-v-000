abcb599047a55b09d27e84953588a06bf0c3795a
Catherine Salazar <catcreates13@gmail.com>
2018-11-28 02:56:05 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,42 +1,35 @@
-require 'pry'
-
 class LandmarksController < ApplicationController
-
   get '/landmarks' do
     @landmarks = Landmark.all
+    @figures = Figure.all
     erb :'landmarks/index'
   end
 
   get '/landmarks/new' do
-
-    erb :'landmarks/new'
+    erb :"landmarks/new"
   end
 
   get '/landmarks/:id' do
-     @landmark = Landmark.find(params[:id])
-     erb :'/landmarks/show'
-   end
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/show"
+  end
 
-   get '/landmark/:id/edit' do
-     @landmark = Landmark.find(params[:id])
-     erb :'/landmarks/edit'
-   end
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/edit"
+  end
 
-  #  post '/landmarks' do
-  #    #binding.pry
-  #     @landmark = Landmark.create(params[:landmark])
-  #     #@landmark.save
-  #     redirect "landmarks/#{@landmark.id}"
-  #   end
+  # edits landmark
+  post '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.name = params['landmark']['name']
+    @landmark.year_completed = params['landmark']['year_completed']
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
+  end
 
-
-  patch '/landmarks/:id' do
-       @landmark = Landmark.find(params[:id])
-       @landmark.update(params[:landmark])
-
-       redirect :"/landmarks/#{@landmark.id}"
-
-   end
-
-
+  post '/landmarks' do
+    Landmark.create(name: params['landmark']['name'], year_completed: params['landmark']['year_completed'])
+    redirect '/landmarks'
+  end
 end
