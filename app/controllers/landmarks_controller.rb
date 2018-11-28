d4ee558de4b67bb54fcd25c19ab6fa56e0962e0d
Catherine Salazar <catcreates13@gmail.com>
2018-11-28 01:49:08 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,10 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+
+  get '/landmarks' do
+    #binding.pry
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+
 end
