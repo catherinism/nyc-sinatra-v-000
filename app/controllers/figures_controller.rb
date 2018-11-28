b0cafa8e3dfdd734da003533130238027632ca71
Catherine Salazar <catcreates13@gmail.com>
2018-11-28 01:38:49 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -40,9 +40,10 @@ class FiguresController < ApplicationController
   patch '/figures/:id' do
     @figure = Figure.find(params[:id])
     @figure.update(params[:figure])
-    @figure
     @landmark = Landmark.find_or_create_by([:landmark])
+    @figure.save
     redirect :"/figures/#{@figure.id}"
+    
   end
 
 
