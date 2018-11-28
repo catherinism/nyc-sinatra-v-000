fda7a61cd5997956061b3a0f70ffd77ccfd00c6e
Catherine Salazar <catcreates13@gmail.com>
2018-11-28 02:59:24 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -22,7 +22,6 @@ class LandmarksController < ApplicationController
      erb :'/landmarks/edit'
    end
 
-
   patch '/landmarks/:id' do
        @landmark = Landmark.find(params[:id])
        @landmark.update(params[:landmark])
